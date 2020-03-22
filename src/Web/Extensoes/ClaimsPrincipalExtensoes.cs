c25c88d6eafbcc3755778decb8368c0e7502c5d2
Matheus da Silva Cruz <matheusvartan@gmail.com>
2020-03-22 15:19:45 -0400

    Adicionada partial para acessar o login

---
+++ src/Web/Extensoes/ClaimsPrincipalExtensoes.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace Web.Extensoes
+{
+    public static class ClaimsPrincipalExtensoes
+    {
+        public static string Nome(this ClaimsPrincipal user) =>
+            user.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value;
+    }
+}
